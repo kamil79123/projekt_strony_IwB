2e32de5ffbd2f90fcfde9f6c360dd8e538181a6c
kamil79123 <kamil79123@gmail.com>
2024-06-20 15:08:29 +0200

    astro api

--- assets/js/astroAPI_handle.js
+++ assets/js/astroAPI_handle.js
@@ -10,7 +10,7 @@ xhr.addEventListener("readystatechange", function () {
 });
 
 xhr.open("GET", "https://api.astronomyapi.com/api/v2/bodies/positions?longitude=-84.39733&latitude=33.775867&elevation=1&from_date=2024-06-20&to_date=2024-06-20&time=15%3A00%3A45");
-xhr.setRequestHeader("Authorization", "Basic MzJmOTNkMjAtNjVkNi00YmVjLWE4YzMtMjM4OWU4ZjBmZGM5Ojc1OTJmODNhZjNkMmIzNzljOGMwY2U4OGE2N2Y4Nzg5YzIyZDczZGFjMjNiMjc0MjU3ZWQ1ZjZkMGU1MTYxY2I0OGZmOGQ3OGNjMDNkMTRlYmNmOGFlOWM5MTkzODdlNTg1NmVlODYxMWE5ZDUyMTkyYTg2OTY5NTExZWJlOGIyYTc3Y2UwMTEwMTI1MTY4ZjlhYzRkYjQyOTliMzVlMTAzMTNjMzRhZmQ1Yjk1YTA5NjcyZjQ5MTc0ZmQ5Zjc4NDRlZmRmODFkOGQ1OWM3ZGMyNGU5NzgyMDMxMWJiYTIw");
+xhr.setRequestHeader("Authorization", "Basic 32f93d20-65d6-4bec-a8c3-2389e8f0fdc9:7592f83af3d2b379c8c0ce88a67f8789c22d73dac23b274257ed5f6d0e5161cb48ff8d78cc03d14ebcf8ae9c919387e5856ee8611a9d52192a86969511ebe8b2a77ce0110125168f9ac4db4299b35e10313c34afd5b95a09672f49174fd9f7844efdf81d8d59c7dc24e97820311bba20");
 
 xhr.send(data);
 console.log(xhr);
